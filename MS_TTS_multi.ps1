822bc2dae57e7a2a294d7d1679664f15f9dc1326
Ray Maslanka <40275577+raymaslanka@users.noreply.github.com>
2023-12-02 17:38:26 -0500

    added CLI argument and error checking

--- MS_TTS_multi.ps1
+++ MS_TTS_multi.ps1
@@ -1,47 +1,71 @@
-# need to retrieve an access token using your subscription key 
-# to later post a request to generate the audio
-$subscriptionKey = '<PUT AZURE TTS SUBSCRIPTION KEY HERE>'
+# require a subscription key as a CLI arguement
+if ($args[0]) {
+    $subscriptionKey = $args[0]
+}
+else {
+    Write-Output ""
+    Write-Output "Please pass an Azure subscription key via command line."
+    Write-Output "Try: PS $PSscriptRoot> ./$($MyInvocation.MyCommand.Name) <YOUR SUBSCRIPTION KEY>"
+    Write-Output ""
+    Exit
+}
 
+
+# get an authorization token using the subscription key
 $FetchTokenHeader = @{
     'Content-type'='application/x-www-form-urlencoded';
     'Content-Length'= '0';
     'Ocp-Apim-Subscription-Key' = $subscriptionKey
 }
+Try {
+    $OAuthToken = Invoke-RestMethod -Method POST -Uri https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken -Headers $FetchTokenHeader
+} Catch {
+    Write-Host "ERROR: There is a problem getting an authorization token:"
+    if($_.ErrorDetails.Message) {
+        Write-Host $_.ErrorDetails.Message
+    } else {
+        Write-Host $_
+    }
+    Write-Host "If a token was retrived recently, this may not be fatal."
+}
 
-$OAuthToken = Invoke-RestMethod -Method POST -Uri https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken -Headers $FetchTokenHeader
 
-$accessKey = $OAuthToken
+# define your MS region, voicename, file codec
 $region = "eastus" 
 $endpoint = "https://$region.tts.speech.microsoft.com"
-
 $voiceName = "en-US-JennyNeural"
-
-# we set our 8bit 8khz requirement here
 $headers = @{
-    "Authorization" = "Bearer: $accessKey"
+    "Authorization" = "Bearer: $OAuthToken"
     "Content-Type" = "application/ssml+xml"
     "User-Agent" = "CrappyTTSScript"
     "X-Microsoft-OutputFormat" = "riff-8khz-8bit-mono-mulaw"
 }
 
+
+# invoke REST API method for each row in CSV
+# return a file name defined in column 1 with audio defined in column 2
 $file = "prompts.csv"
 $data = Import-Csv $file
-
 foreach ($row in $data) {
     $value1 = $row.Column1
     $value2 = $row.Column2
-
+    # file extension here (.wav, .mp3, etc) should match X-Microsoft-OutputFormat above
     $outputFile = "$value1.wav"
     $text = $value2
 
     $requestBody = @"
     <speak version='1.0' xml:lang='en-US'><voice xml:lang='en-US' name='$voiceName'>$text</voice></speak>
 "@
-
-    # Invoke the REST API to generate speech
-    Write-Host $endpoint/cognitiveservices/v1
-    $response = Invoke-RestMethod -Uri "$endpoint/cognitiveservices/v1" -Headers $headers -Method POST -Body $requestBody -OutFile $outputFile
-
-    Write-Host "Speech synthesis completed. Output file: $outputFile"
+    Try {
+        $response = Invoke-RestMethod -Uri "$endpoint/cognitiveservices/v1" -Headers $headers -Method POST -Body $requestBody -OutFile $outputFile
+        Write-Host "Speech synthesis completed. Output file: $outputFile"
+    } Catch {
+        Write-Host "ERROR: There is a problem requesting the file $outputFile :"
+        if($_.ErrorDetails.Message) {
+            Write-Host $_.ErrorDetails.Message
+        } else {
+            Write-Host $_
+        }
+    }
 }
 
